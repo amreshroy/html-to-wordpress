63f0fa9e1c0c3e7478f4d5264825263b8d17c69f
amreshroy <amreshchandraroy@gmail.com>
2022-06-17 19:28:21 +0600

    single page template added

--- neuron_finance/header.php
+++ neuron_finance/header.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<?php wp_head(); ?>
+	</head>
+
+	<body>
         <header>
 			<!-- start top bar -->
 			<div class="header-top-area">
